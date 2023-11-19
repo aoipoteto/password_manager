85710a38d9ce682ede64a6d79f36f33886040fa0
aoipoteto <tani.aoi3434@gmail.com>
2023-11-19 16:59:23 +0900

    不要なコメントを削除した

--- password_manager.sh
+++ password_manager.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#ようこそ書く
-echo "パスワードマネージャーへようこそ！"
+echo "パスワードマネージャーへようこそ!"
 while true; do
     echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
     read choice
 
     if [ "$choice" == "Add Password" ]; then
-        #addPasswordが入力された場合
         echo "サービス名を入力してください："
         read service
         echo "ユーザー名を入力してください："
@@ -15,9 +13,6 @@ while true; do
         read password
         echo "$service:$username:$password" >> password.txt
 
-   
-
-    #getが入力された場合
     elif [ "$choice" == "Get Password" ]; then
         echo "サービス名を入力してください"
         read research_service
@@ -36,20 +31,12 @@ while true; do
         if [ "$registered" == false ]; then
             echo "そのサービスは登録されていません。"
         fi
-        
 
-        #保存されているサービス名、ユーザー名、パスワードの３つを表示する
-
-
-    #Exitが入力された場合
     elif [ "$choice" == "Exit" ]; then
         echo "Thank you!"
         break
 
-
-    #その他が入力された場合 
     else
         echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
     fi
-   
 done
