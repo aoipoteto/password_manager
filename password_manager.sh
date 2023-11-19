50f9c4e0705b17b1338c4506e17b940810795fb8
aoipoteto <tani.aoi3434@gmail.com>
2023-11-19 16:49:50 +0900

    パスワードマネージャー完成

--- password_manager.sh
+++ password_manager.sh
@@ -1,40 +1,55 @@
 #!/bin/bash
 #ようこそ書く
 echo "パスワードマネージャーへようこそ！"
-echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
-read choice
-#選択肢を選ぶif使う
-if [ "$choice" == "Add Password" ]; then
-    #addPasswordが入力された場合
-    echo "サービス名を入力してください："
-    read service
-    echo "ユーザー名を入力してください："
-    read username
-    echo "パスワードを入力してください："
-    read password
-    echo "$service:$username:$password" >> password.txt
+while true; do
+    echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
+    read choice
 
-#選択肢を選ぶとこに戻る
+    if [ "$choice" == "Add Password" ]; then
+        #addPasswordが入力された場合
+        echo "サービス名を入力してください："
+        read service
+        echo "ユーザー名を入力してください："
+        read username
+        echo "パスワードを入力してください："
+        read password
+        echo "$service:$username:$password" >> password.txt
 
-#getが入力された場合
-elif [ "$choice" == "Get Password" ]; then
-    echo "サービス名を入力してください"
-    ##サービスめが保存されていなかった場合
-    echo "そのサービスは登録されていません。"
-    ##サービスが保存されていた場合
-    #保存されているサービス名、ユーザー名、パスワードの３つを表示する
+   
 
-##選択肢を選ぶとこに戻る
+    #getが入力された場合
+    elif [ "$choice" == "Get Password" ]; then
+        echo "サービス名を入力してください"
+        read research_service
 
-#Exitが入力された場合
-elif [ "$choice" == "Exit" ]; then
-    echo "Thank you!"
+        registered=false
+        while IFS=':' read -r service username password; do
+            if [ "$research_service" == "$service" ]; then
+                echo "サービス名:$service"
+                echo "ユーザー名:$username"
+                echo "パスワード:$password"
+                registered=true
+                break
+            fi
+        done < password.txt
+       
+        if [ "$registered" == false ]; then
+            echo "そのサービスは登録されていません。"
+        fi
+        
 
-#プログラムが終了
+        #保存されているサービス名、ユーザー名、パスワードの３つを表示する
 
 
-#その他が入力された場合 
-else
-    echo "入力が間違えています。(Add Password/GetPassword/Exit) :"
-fi
-#選択肢を選ぶとこに戻る
+    #Exitが入力された場合
+    elif [ "$choice" == "Exit" ]; then
+        echo "Thank you!"
+        break
+
+
+    #その他が入力された場合 
+    else
+        echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+    fi
+   
+done
